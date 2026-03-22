5a1f4679a96e471f715229563bd8b24f9f1f16cf
naumsarti <naumsarti@gmail.com>
2026-03-22 20:40:09 -0300

    Adicionado o código para controle de pagamento

---
+++ controle_pagamento.py
@@ -0,0 +1,24 @@
+import datetime
+
+class Pagamento:
+    def __init__(self, pagador, categoria, curso, valor):
+        self.pagador = pagador
+        self.categoria = categoria  # Aluno, Servidor ou Professor [cite: 14]
+        self.curso = curso          # IA ou ESG [cite: 14]
+        self.valor = valor
+        self.data_hora = datetime.datetime.now()
+        self.proximo = None
+
+class HistoricoPagamentos:
+    def __init__(self):
+        self.head = None
+
+    def registrar_pagamento(self, pagamento):
+        if not self.head:
+            self.head = pagamento
+        else:
+            atual = self.head
+            while atual.proximo:
+                atual = atual.proximo
+            atual.proximo = pagamento
+        print(f"Pagamento de R${pagamento.valor} por {pagamento.pagador} registrado via PIX.")
