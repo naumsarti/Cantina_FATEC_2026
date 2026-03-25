bce123ebbd8534ca7116001562bbd6faf6b36a66
naumsarti <naumsarti@gmail.com>
2026-03-25 10:46:42 -0300

    Código consumo

--- controle_consumo.py
+++ controle_consumo.py
@@ -1,10 +1,20 @@
 class Consumo:
     def __init__(self, pagador, produto_nome, quantidade, valor_total):
-        self.pagador = pagador
-        self.produto_nome = produto_nome
-        self.quantidade = quantidade
-        self.valor_total = valor_total
+        self._pagador = pagador
+        self._produto_nome = produto_nome
+        self._quantidade = quantidade
+        self._valor_total = valor_total
         self.proximo = None
+
+
+    @property
+    def pagador(self): return self._pagador
+    @property
+    def produto_nome(self): return self._produto_nome
+    @property
+    def quantidade(self): return self._quantidade
+    @property
+    def valor_total(self): return self._valor_total
 
 class GerenciadorConsumo:
     def __init__(self):
@@ -19,3 +29,4 @@ class GerenciadorConsumo:
             while atual.proximo:
                 atual = atual.proximo
             atual.proximo = novo_consumo
+        return novo_consumo
