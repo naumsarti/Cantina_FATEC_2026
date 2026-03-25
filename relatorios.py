3b156eb3bc95f2d58c42427d3f865491ed2ec05a
naumsarti <naumsarti@gmail.com>
2026-03-25 10:49:42 -0300

    Código relatórios

--- relatorios.py
+++ relatorios.py
@@ -3,41 +3,27 @@ from controle_pagamento import HistoricoPagamentos
 from controle_consumo import GerenciadorConsumo
 
 class RelatorioService:
-    """Gera resumos formatados das atividades da Cantina."""
-
     def gerar_relatorio_vendas(self, estoque: Estoque):
         print(f"\n{'='*20} ESTOQUE ATUAL {'='*20}")
-        
-        atual, total_geral = estoque.head, 0
-        if not atual:
-            print("Estoque vazio.")
+        atual = estoque.head
+        if not atual: print("Estoque vazio.")
         
         while atual:
-            print(f"• {atual}") # Usa o __str__ de Produto
-            total_geral += atual.quantidade
+            print(f"• {atual.nome} | Preço: R${atual.preco_venda:.2f} | Qtd: {atual.quantidade}")
             atual = atual.proximo
-            
-        print(f"{'-'*55}\nTotal de itens: {total_geral}\n{'='*55}")
 
     def gerar_relatorio_consumo(self, consumo: GerenciadorConsumo, pagamentos: HistoricoPagamentos):
         print(f"\n{'='*15} RESUMO DE CONSUMO E CAIXA {'='*15}")
-
-        # Seção de Consumo
-        print("\n[PRODUTOS CONSUMIDOS]")
+        
         item = consumo.head
-        if not item: print("Nenhum registro.")
         while item:
-            print(f"- {item.pagador}: {item.quantidade}x {item.produto_nome} (R${item.valor_total:.2f})")
+            print(f"- {item.pagador} consumiu {item.produto_nome} (Total: R${item.valor_total:.2f})")
             item = item.proximo
 
-        # Seção de Pagamentos
-        print("\n[ENTRADAS PIX]")
-        pg, total_caixa = pagamentos.head, 0
-        if not pg: print("Nenhum pagamento.")
+        pg = pagamentos.head
+        total_caixa = 0
         while pg:
-            data_pg = pg.data_hora.strftime('%d/%m/%Y %H:%M')
-            print(f"- {pg.pagador} ({pg.categoria}): R${pg.valor:.2f} | {data_pg}")
+            print(f"- PGTO: {pg.pagador} ({pg.curso}) | R${pg.valor:.2f}")
             total_caixa += pg.valor
             pg = pg.proximo
-
-        print(f"{'-'*55}\nSALDO TOTAL EM CAIXA: R${total_caixa:.2f}\n{'='*55}")
+        print(f"SALDO TOTAL EM CAIXA: R${total_caixa:.2f}")
