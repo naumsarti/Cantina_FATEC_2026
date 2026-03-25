1283efa807647e355e64b7e727cdda2a16a4d85a
naumsarti <naumsarti@gmail.com>
2026-03-25 10:45:30 -0300

    Código estoque

--- controle_estoque.py
+++ controle_estoque.py
@@ -1,27 +1,42 @@
 import datetime
 
-# definição da classe produto para adicionar e remover
 class Produto:
     def __init__(self, nome, preco_compra, preco_venda, data_vencimento, quantidade):
-        self.nome = nome
-        self.preco_compra = preco_compra
-        self.preco_venda = preco_venda
-        self.data_vencimento = data_vencimento 
-        self.data_compra = datetime.datetime.now()
-        self.quantidade = quantidade
+        self._nome = nome
+        self._preco_compra = preco_compra
+        self._preco_venda = preco_venda
+        self._data_vencimento = data_vencimento 
+        self._data_compra = datetime.datetime.now()
+        self._quantidade = quantidade
         self.proximo = None  
 
+
+    @property
+    def nome(self): return self._nome
+    @property
+    def preco_compra(self): return self._preco_compra
+    @property
+    def preco_venda(self): return self._preco_venda
+    @property
+    def data_vencimento(self): return self._data_vencimento
+    @property
+    def quantidade(self): return self._quantidade
+
+    @quantidade.setter
+    def quantidade(self, valor):
+        if valor >= 0:
+            self._quantidade = valor
+        else:
+            print("Erro: Quantidade não pode ser negativa.")
+
     def __str__(self):
-        return f"{self.nome} | Qtd: {self.quantidade} | Vence: {self.data_vencimento}"
+        return f"{self._nome} | Qtd: {self._quantidade} | Vence: {self._data_vencimento}"
 
-
-#     
 class Estoque:
     def __init__(self):
-        self.head = None  # Início da lista (item mais antigo)
+        self.head = None
 
     def adicionar_produto(self, produto):
-        """Adiciona um produto ao final da lista (estoque novo)"""
         if not self.head:
             self.head = produto
         else:
@@ -29,21 +44,13 @@ class Estoque:
             while atual.proximo:
                 atual = atual.proximo
             atual.proximo = produto
-        print(f"Produto {produto.nome} adicionado ao estoque.")
 
     def vender_item(self, nome_produto, quantidade_venda):
-        """Lógica de saída: remove do mais antigo para o mais novo"""
         atual = self.head
         while atual:
             if atual.nome == nome_produto and atual.quantidade > 0:
                 if atual.quantidade >= quantidade_venda:
                     atual.quantidade -= quantidade_venda
-                    print(f"Venda realizada! Restam {atual.quantidade} de {nome_produto}.")
                     return True
-                else:
-                    print("Quantidade insuficiente neste lote!")
-                    return False
             atual = atual.proximo
-        print("Produto não encontrado.")
         return False
-    
